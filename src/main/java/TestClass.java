55eb473ed947fe26c4143f4ce1d620baf5d152bb
DieuLinh <19521751@gm.uit.edu.vn>
2023-03-08 11:19:59 +0700

    first commit

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,41 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import static org.testng.Assert.assertEquals;
+
+public class TestClass {
+    ChromeDriver chromeDriver;
+    @BeforeMethod
+   public void Setup(){
+       System.out.println("Before Method");
+        // Setup data
+
+        WebDriverManager.chromedriver().setup();
+        chromeDriver = new ChromeDriver();
+   }
+    @Test
+    public void Run(){
+        //body Test scripts
+        chromeDriver.get("https://www.swim.com/");
+        sleep(5000);
+
+    }
+
+   @AfterMethod
+   public void CleanUp() {
+       System.out.println("After Method");
+       //Cleanup Data
+       chromeDriver.quit();
+   }
+
+   private void sleep(int time){
+        try{
+            Thread.sleep(time);
+            }
+        catch(Exception ex){
+            System.out.print(ex.getMessage());
+        }
+   }
+}
+
