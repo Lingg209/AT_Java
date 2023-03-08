b2342410186c990b9a336890810984f0d6588e61
DieuLinh <19521751@gm.uit.edu.vn>
2023-03-08 13:46:46 +0700

    getElement

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,7 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 
@@ -19,15 +24,25 @@ public class TestClass {
     @Test
     public void Run(){
         //body Test scripts
-        chromeDriver.get("https://www.swim.com/");
-        sleep(6000);
+        chromeDriver.get("https://auto.fresher.dev/lessons/lession7/index.html");
+        WebElement loginButton;
+        loginButton = chromeDriver.findElement(By.className("btn-success"));
+        //findElement: when condition is just only
+        List<WebElement> buttons = chromeDriver.findElements(By.className("btn-success"));
+        for(int i = 0; i < buttons.size(); i++)
+        {
+            buttons.get(i).click();
+            sleep(3000);
+        }
+        //findElements: when we want to get many element has same condition filter
     }
+
 
    @AfterMethod
    public void CleanUp() {
        System.out.println("After Method");
        //Cleanup Data
-       chromeDriver.quit();
+      // chromeDriver.quit();
    }
 
    private void sleep(int time){
