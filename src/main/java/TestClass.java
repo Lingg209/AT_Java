24611b4f4c62675f535761f7665cf13049b09407
DieuLinh <19521751@gm.uit.edu.vn>
2023-03-10 14:25:15 +0700

    Test scripts for disable field, Number field, Input field

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -13,6 +15,9 @@ import static org.testng.Assert.assertEquals;
 
 public class TestClass {
     ChromeDriver chromeDriver;
+    String input = "q&a123.456";
+    String expect = "123.456";
+
     @BeforeMethod
    public void Setup(){
        System.out.println("Setup Method");
@@ -24,8 +29,23 @@ public class TestClass {
     public void Run(){
         //body Test scripts
         chromeDriver.get("https://auto.fresher.dev/lessons/lession7/index.html");
+        sleep(3000);
 
-        WebElement button;
+        WebElement button = chromeDriver.findElement(By.id("btnExample1"));
+        button.click();
+        WebElement lbStatusButton = chromeDriver.findElement(By.id("lbStatusButton"));
+        String lbStatusButtonValue = lbStatusButton.getText();
+        Assert.assertEquals(lbStatusButtonValue,"Click on Button 1");
+
+        WebElement textInput = chromeDriver.findElement(By.id("txtInput1"));
+        String textInputValue = textInput.getAttribute("value");
+        Assert.assertEquals(textInputValue,"Default value of input");
+        textInput.clear();
+        textInput.sendKeys("New value of input");
+        String textInputNewValue = textInput.getAttribute("value");
+        Assert.assertEquals(textInputNewValue,"New value of input");
+
+        /*WebElement button;
         button = chromeDriver.findElement(By.className("btn-success"));
 
         //findElement: when condition is just only
@@ -53,8 +73,6 @@ public class TestClass {
         textBox.clear();
         action.sendKeys(textBox, "abc").build().perform();
 
-
-
         List<WebElement> buttons = chromeDriver.findElements(By.className("btn-success"));
         for(int i = 0; i < buttons.size(); i++)
         {
@@ -62,16 +80,52 @@ public class TestClass {
             sleep(3000);
         }
         //findElements: when we want to get many element has same condition filter
-
+*/
 
     }
 
+    @Test
+    public void Run2_disableField()
+    {
+        chromeDriver.get("https://auto.fresher.dev/lessons/lession7/index.html");
+        sleep(3000);
+
+        WebElement disabled = chromeDriver.findElement(By.id("txtInput2"));
+
+        removeAttribute(disabled,"disabled");
+        disabled.clear();
+        disabled.sendKeys("New value of input disable");
+        setAttribute(disabled,"disabled");
+
+        String disableValue = disabled.getAttribute("value");
+        Assert.assertEquals(disableValue,"New value of input disable");
+    }
+
+    @Test
+    public void Run3_number(){
+        chromeDriver.get("https://auto.fresher.dev/lessons/lession7/index.html");
+        sleep(3000);
+
+        WebElement number = chromeDriver.findElement(By.id("txtInput3"));
+        number.clear();
+        number.sendKeys(input);
+        sleep(300);
+        input = number.getAttribute("value");
+        Assert.assertEquals(input,expect);
+
+    }
+    public void removeAttribute( WebElement element ,String attr){
+        ((JavascriptExecutor) chromeDriver).executeScript("arguments[0].removeAttribute('"+ attr +"')",element);
+    }
+    public void setAttribute( WebElement element, String attr){
+        ((JavascriptExecutor) chromeDriver).executeScript("arguments[0].setAttribute('"+ attr +"','')",element);
+    }
 
    @AfterMethod
    public void CleanUp() {
        System.out.println("After Method");
        //Cleanup Data
-      // chromeDriver.quit();
+      chromeDriver.quit();
    }
 
    private void sleep(int time){
